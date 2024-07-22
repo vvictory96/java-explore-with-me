74e8e89afb0ca87e5d7d57cd3490210600e9b367
Vlasenko Viktoriya <vvlasenko@lanit.ru>
2024-07-22 22:23:18 +0300

    stat_svc 1 version some edit

--- stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/server/service/StatsService.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface StatsService {
 
     void postHit(EndpointHit hit);
+
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
